18cad9d9263c72645e0a18f498408b7c9caac21b
Peplinn <chidiebubeoluoma01@gmail.com>
2024-06-11 16:51:51 +0100

    Initial Commit: Added files from old project directory.

---
+++ android/app/src/main/kotlin/com/example/owa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.owa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:owa/pages/map_page.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(
+    Duration(seconds: 5),
+  );
+  FlutterNativeSplash.remove();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MapPage(),
+    );
+  }
+}
